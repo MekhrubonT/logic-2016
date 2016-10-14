b5da448dbd1af415b54c6a5640c36eddd811c585
Mekhrubon <mehrubon.97@mail.ru>
2016-10-15 02:52:26 +0300

    new modus ponens

--- logic-2016-hw1/src/LogicHW1.java
+++ logic-2016-hw1/src/LogicHW1.java
@@ -1,13 +1,16 @@
+import jdk.internal.util.xml.impl.Pair;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LogicHW1 {
-    final static String fileName = "D:\\logic-2016\\logic-2016-hw1\\HW1\\good6.in";
+    final static String fileName = "D:\\logic-2016\\logic-2016-hw1\\HW1\\good5.in";
     static Parser p = new Parser();
-    static Map<String, Integer> data;
-    static HashMap<String, Integer> assumption;
+    static Map<Expression, Integer> data;
+    static Map<Expression, ArrayList<Pair>> conRightParts;
+    static HashMap<Expression, Integer> assumption;
     static ArrayList<Expression> expressions;
     static PrintWriter out;
     static BufferedReader in;
@@ -25,8 +28,9 @@ public class LogicHW1 {
     }};
 
     static boolean isAxiom(Expression d) {
+//        System.out.println("Checking " + d);
         for (int i = 0; i < axioms.size(); i++) {
-            if (axioms.get(i).equalStruct(d, new HashMap<>())) {
+            if (axioms.get(i).equalStruct(d, new HashMap<>(), true)) {
                 out.println("Сх. акс. " + (i + 1));
                 return true;
             }
@@ -35,15 +39,26 @@ public class LogicHW1 {
     }
 
     static private boolean modusPonens(Expression b) {
+/*
         for (int i = 0; i < expressions.size(); i++) {
             Expression cur = expressions.get(i);
             if (cur.instance == BinaryOperation.BINARYOPERATION) {
                 BinaryOperation spec = (BinaryOperation) cur;
                 if (spec.op == BinaryOperation.Operation.CON) {
-                    if (data.containsKey(spec.lhs.toString()) && spec.rhs.hashCode() == b.hashCode()) {
-                        out.println("M.P. " + data.get(spec.lhs.toString()) + ", " + (i + 1));
+                    if (data.containsKey(spec.lhs) && spec.rhs.hashCode() == b.hashCode()) {
+                        out.println("M.P. " + data.get(spec.lhs) + ", " + (i + 1));
                         return true;
                     }
+                }
+            }
+        }
+*/
+        if (conRightParts.containsKey(b)) {
+            ArrayList<Pair> arr = conRightParts.get(b);
+            for (Pair pair : arr) {
+                if (data.containsKey(pair.d)) {
+                    out.println("M.P. " + data.get(pair.d) + ", " + pair.id);
+                    return true;
                 }
             }
         }
@@ -51,44 +66,70 @@ public class LogicHW1 {
     }
 
     public static boolean isAssumption(Expression d) {
-        if (assumption.containsKey(d.toString())) {
-            out.println(assumption.get(d.toString()) + 1);
+        if (assumption.containsKey(d)) {
+            out.println(assumption.get(d) + 1);
             return true;
         }
         return false;
     }
 
     public static void main(String[] args) throws IOException {
-        long time = System.currentTimeMillis();
         in = new BufferedReader(new FileReader(fileName));
         out = new PrintWriter(new File("output.txt"));
         data = new HashMap<>();
         assumption = new HashMap<>();
+        conRightParts = new HashMap<>();
         expressions = new ArrayList<>();
+
         int ind = 0;
-////        String title = in.readLine();
-////        StringTokenizer t = new StringTokenizer(title);
-////        while (t.hasMoreTokens()) {
-////            title = t.nextToken();
-////            if (title.equals("|-")) {
-////                break;
-////            }
-////            Expression d = p.parse(title);
-////            assumption.put(d, ind);
-////            ind++;
-////        }
-//
+//        String title = in.readLine();
+//        StringTokenizer t = new StringTokenizer(title);
+//        while (t.hasMoreTokens()) {
+//            title = t.nextToken();
+//            if (title.equals("|-")) {
+//                break;
+//            }
+//            Expression d = p.parse(title);
+//            assumption.put(d, ind);
+//            ind++;
+//        }
+
         ind = 0;
         for (String cur = in.readLine(); cur != null; cur = in.readLine()) {
+//            System.out.println(ind);
+//            System.out.println(cur);
             Expression d = p.parse(cur);
             if (!isAxiom(d) && !isAssumption(d) && !modusPonens(d)) {
                 out.println("Не доказано");
             }
             ind++;
+
             expressions.add(d);
-            data.put(d.toString(), ind);
+            data.put(d, ind);
+            if (d.instance == BinaryOperation.BINARYOPERATION) {
+                BinaryOperation bd = (BinaryOperation) d;
+                if (bd.op == BinaryOperation.Operation.CON) {
+                    if (!conRightParts.containsKey(bd.rhs)) {
+                        conRightParts.put(bd.rhs, new ArrayList<>());
+                    }
+                    conRightParts.get(bd.rhs).add(new Pair(bd.lhs, ind));
+                }
+            }
+            if (ind % 1000 == 0)
+                System.out.println(ind);
         }
+
+
         out.close();
-        System.out.println(System.currentTimeMillis() - time);
+    }
+
+
+    static class Pair {
+        Expression d;
+        int id;
+        Pair(Expression d, int id) {
+            this.d = d;
+            this.id = id;
+        }
     }
 }
